646fd064b3a1fb8d1aad1b7384a9fce05888ec85
Huong Nguyen <65859562+thihuong1010@users.noreply.github.com>
2023-07-06 21:52:43 +0300

    Specify singular table names in the DbContext

--- ContosoUniversity/Data/SchoolContext.cs
+++ ContosoUniversity/Data/SchoolContext.cs
@@ -12,6 +12,14 @@ namespace ContosoUniversity.Data
 		public DbSet<Course> Courses { get; set; }
 		public DbSet<Enrollment> Enrollments { get; set; }
 		public DbSet<Student> Students { get; set; }
-	}
+
+		// Specify singular table names in the DbContext
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+			modelBuilder.Entity<Course>().ToTable("Course");
+			modelBuilder.Entity<Enrollment>().ToTable("Enrollment");
+			modelBuilder.Entity<Startup>().ToTable("Student");
+        }
+    }
 }
 
