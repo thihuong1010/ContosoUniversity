f0aecdd98e83c70e04cf6404f74753e75db2db9f
Huong Nguyen <65859562+thihuong1010@users.noreply.github.com>
2023-07-06 23:04:31 +0300

    Add students controller scaffolding
    Scaffolder MVC controller with views, using Entity Framework

--- ContosoUniversity/Data/SchoolContext.cs
+++ ContosoUniversity/Data/SchoolContext.cs
@@ -3,23 +3,21 @@ using Microsoft.EntityFrameworkCore;
 
 namespace ContosoUniversity.Data
 {
-	public class SchoolContext : DbContext
-	{
-		public SchoolContext(DbContextOptions<SchoolContext> options) : base(options)
-		{
-		}
+    public class SchoolContext : DbContext
+    {
+        public SchoolContext(DbContextOptions<SchoolContext> options) : base(options)
+        {
+        }
 
-		public DbSet<Course> Courses { get; set; }
-		public DbSet<Enrollment> Enrollments { get; set; }
-		public DbSet<Student> Students { get; set; }
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Enrollment> Enrollments { get; set; }
+        public DbSet<Student> Students { get; set; }
 
-		// Specify singular table names in the DbContext
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-			modelBuilder.Entity<Course>().ToTable("Course");
-			modelBuilder.Entity<Enrollment>().ToTable("Enrollment");
-			modelBuilder.Entity<Startup>().ToTable("Student");
+            modelBuilder.Entity<Course>().ToTable("Course");
+            modelBuilder.Entity<Enrollment>().ToTable("Enrollment");
+            modelBuilder.Entity<Student>().ToTable("Student");
         }
     }
 }
-
