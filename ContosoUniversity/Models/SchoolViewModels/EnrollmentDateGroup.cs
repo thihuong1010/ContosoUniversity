02a35bbc516123ec021dcb616caf91ce1ca6bda1
Huong Nguyen <65859562+thihuong1010@users.noreply.github.com>
2023-07-16 20:33:53 +0300

    Create an About page
    - Create EnrollmentDateGroup class
    - Modify the Home controller: Add About method

---
+++ ContosoUniversity/Models/SchoolViewModels/EnrollmentDateGroup.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+// Create a view model class for the data that need to pass to the view
+namespace ContosoUniversity.Models.SchoolViewModels
+{
+	public class EnrollmentDateGroup
+	{
+		[DataType(DataType.Date)]
+		public DateTime? EnrollmentDate { get; set; }
+
+		public int StudentCount { get; set; }
+	}
+}
+
